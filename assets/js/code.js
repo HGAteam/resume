956cacc4ac48d1116e06649dfd12a931ec45a43f
HGA Team <54682400+HGAteam@users.noreply.github.com>
2023-02-17 15:29:52 -0300

    Nueva idea de curriculum

---
+++ assets/js/code.js
@@ -0,0 +1,4 @@
+// var full_name = document.querySelector('span ["class="full_name"]');
+// var age = document.querySelector('span ["class="age"]');
+// var address = document.querySelector('span ["class="address"]');
+// var email = document.querySelector('span ["class="email"]');
