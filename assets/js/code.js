d8d55a434154e30587ee382185fdb13ff06e683a
HGA Team <54682400+HGAteam@users.noreply.github.com>
2023-02-23 20:08:31 -0300

    New Data to show in cards

--- assets/js/code.js
+++ assets/js/code.js
@@ -2,3 +2,38 @@
 // var age = document.querySelector('span ["class="age"]');
 // var address = document.querySelector('span ["class="address"]');
 // var email = document.querySelector('span ["class="email"]');
+$(document).ready(function () {
+  var img = document.querySelector("img");
+  var age = document.getElementById("age");
+
+  calcularEdad()
+
+  $.ajax({
+    url: "https://randomuser.me/api/",
+    dataType: "json",
+    success: function (data) {
+      let results = data.results;
+      results.forEach((result) => {
+        img.src = result.picture.large;
+        console.log(result.dob.date);
+      });
+    },
+  });
+
+  function calcularEdad() {
+    var hoy = new Date();
+    var cumpleanos = new Date('06/05/1989');
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+      edad--;
+    }
+
+    return age.innerText = edad;
+  }
+
+  function changeInfo(){
+    
+  }
+});
